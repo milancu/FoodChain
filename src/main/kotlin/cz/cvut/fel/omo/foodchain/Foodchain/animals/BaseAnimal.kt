8cd1089d89fce04615822ca3971cf2d10536014d
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2021-12-20 09:54:00 +0100

    animal eat

--- src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/animals/BaseAnimal.kt
+++ src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/animals/BaseAnimal.kt
@@ -1,19 +1,25 @@
 package cz.cvut.fel.omo.foodchain.Foodchain.animals
 
+import cz.cvut.fel.omo.foodchain.Foodchain.parties.Farmer
 import cz.cvut.fel.omo.foodchain.Foodchain.products.Crop
 
 open class BaseAnimal {
     private var weight : Double // TODO val podle toho, jestli budeme pracovat s vyvojem zvirete
     private val feed : List<Crop>
+    private var foodConsumption : Int
     private var age : Int
     private var price : Int
 
-    constructor(weight: Double, feed: List<Crop>, age: Int, price : Int) {
+    constructor(weight: Double, feed: List<Crop>, foodConsumption : Int, age: Int, price : Int) {
         this.weight = weight
         this.feed = feed
+        this.foodConsumption = foodConsumption
         this.age = age
         this.price = price
     }
 
     // funkce eat
+    fun eatFeed(owner : Farmer, feed : Crop){
+        owner.
+    }
 }
--- src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/parties/Farmer.kt
+++ src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/parties/Farmer.kt
@@ -1,5 +1,20 @@
 package cz.cvut.fel.omo.foodchain.Foodchain.parties
 
+import cz.cvut.fel.omo.foodchain.Foodchain.products.Crop
+
 class Farmer(subjectName : String, identier : Int, location : String, amountOfMoney : Int)
     : BaseParty(subjectName, identier, location, amountOfMoney) {
+
+
+    private var resources : List<Crop> = setInitialResources()
+
+    fun setInitialResources() : List<Crop>{
+             var initialResources : List<Crop> = TODO()
+             // TODO podle potravin
+             return initialResources
+       }
+
+    set(feed : Crop){
+
+    }
 }

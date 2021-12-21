3921a78160a7784d6a518c23929d2400b2a107b0
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2021-12-21 17:07:21 +0100

    add origin ID, fix Factory

--- src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/parties/Butcher.kt
+++ src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/parties/Butcher.kt
@@ -13,22 +13,22 @@ class Butcher {
             when (animal.getName()) {
                 "Cow" -> {
                     val shopPrice = 0.7 * animal.getWeight() * MeatType.BEEF.price
-                    meatList.add(Meat(MeatType.BEEF, shopPrice, shopPrice * 1.4, 0.7 * animal.getWeight()))
+                    meatList.add(Meat(MeatType.BEEF, shopPrice, shopPrice * 1.4, 0.7 * animal.getWeight(), animal.getOriginId()))
                 }
 
                 "Pig" -> {
                     val shopPrice = 0.7 * animal.getWeight() * MeatType.PORK.price
-                    meatList.add(Meat(MeatType.PORK, shopPrice, shopPrice * 1.4, 0.7 * animal.getWeight()))
+                    meatList.add(Meat(MeatType.PORK, shopPrice, shopPrice * 1.4, 0.7 * animal.getWeight(), animal.getOriginId()))
                 }
 
                 "Chicken" -> {
                     val shopPrice = 0.7 * animal.getWeight() * MeatType.CHICKEN.price
-                    meatList.add(Meat(MeatType.CHICKEN, shopPrice, shopPrice * 1.4, 0.7 * animal.getWeight()))
+                    meatList.add(Meat(MeatType.CHICKEN, shopPrice, shopPrice * 1.4, 0.7 * animal.getWeight(), animal.getOriginId()))
                 }
 
                 "Fish" -> {
                     val shopPrice = 0.7 * animal.getWeight() * MeatType.FISH.price
-                    meatList.add(Meat(MeatType.FISH, shopPrice, shopPrice * 1.4, 0.7 * animal.getWeight()))
+                    meatList.add(Meat(MeatType.FISH, shopPrice, shopPrice * 1.4, 0.7 * animal.getWeight(), animal.getOriginId()))
                 }
             }
         }

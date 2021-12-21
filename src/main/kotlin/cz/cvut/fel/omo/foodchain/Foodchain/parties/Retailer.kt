e3ed76e763b2d34ff7e232c86486d7494d66044e
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2021-12-21 14:57:02 +0100

    warehouse movement

--- src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/parties/Retailer.kt
+++ src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/parties/Retailer.kt
@@ -1,6 +1,7 @@
 package cz.cvut.fel.omo.foodchain.Foodchain.parties
 
 import cz.cvut.fel.omo.foodchain.Foodchain.Invoice
+import cz.cvut.fel.omo.foodchain.Foodchain.enums.ProductType
 import cz.cvut.fel.omo.foodchain.Foodchain.products.Crop
 import cz.cvut.fel.omo.foodchain.Foodchain.products.Product
 
@@ -8,6 +9,7 @@ class Retailer(subjectName : String, identier : Int, location : String, amountOf
     : BaseParty(subjectName, identier, location, amountOfMoney) {
 
     private var warehouse : Warehouse = Warehouse() //TODO odecet financi za plnost skladu
+    private var availableProducts : ArrayList<Product> = ArrayList()
     private var unpaidInvoices : ArrayList<Invoice> = ArrayList()
 
     fun buyProducts(products : ArrayList<Product>){
@@ -23,6 +25,24 @@ class Retailer(subjectName : String, identier : Int, location : String, amountOf
             unpaidInvoices.add(invoice)
         }
     }
+
+    fun vacateWarehouse(){
+        for(product in warehouse.getStoragedProducts()){
+            availableProducts.add(product)
+        }
+    }
+
+/*    fun fillIn(){
+        for(product in warehouse.)
+    }*/
+
+    fun fillIn(type : ProductType){
+        var newProducts : ArrayList<Product> = warehouse.getSpecificProducts(type)
+        for(product in newProducts){
+            availableProducts.add(product)
+        }
+    }
+
 
 
 }

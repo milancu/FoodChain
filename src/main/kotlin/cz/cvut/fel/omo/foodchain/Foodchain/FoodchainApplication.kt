ce0c14c3d93cf747cfb5f0275a97655694e8a253
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2021-12-22 18:42:16 +0100

    oprava konfliktu

--- src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/FoodchainApplication.kt
+++ src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/FoodchainApplication.kt
@@ -18,23 +18,6 @@ fun main(args: Array<String>) {
 		println()
 		simulation.runWeek()
 	}
-/*<<<<<<< HEAD
-=======*/
-
-	/*var generator = Generator()
-
-	generator.generateAnimals()
->>>>>>> 7b473f66a356e0fe848bc070448b93ade7420904
 
 	Report.export()
-	println("####################")
-*/
-
-	/*println("#############################################################")
-	var generator : Generator = Generator()
-
-	for(i in generator.generateAnimals()){
-		var id = i.getOriginId()
-		println(Report.getReport(id)!!.get(0))
-	}*/
 }

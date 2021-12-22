a676e489cd67b51d4d50444d855a0a5e645c6daa
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2021-12-22 10:45:36 +0100

    add observer to field

--- src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/Field.kt
+++ src/main/kotlin/cz/cvut/fel/omo/foodchain/Foodchain/Field.kt
@@ -1,20 +1,29 @@
 package cz.cvut.fel.omo.foodchain.Foodchain
 
+import cz.cvut.fel.omo.foodchain.Foodchain.Observer.Observer
+import cz.cvut.fel.omo.foodchain.Foodchain.Observer.Subject
+import cz.cvut.fel.omo.foodchain.Foodchain.animals.BaseAnimal
 import cz.cvut.fel.omo.foodchain.Foodchain.parties.Grower
 import cz.cvut.fel.omo.foodchain.Foodchain.products.Crop
+import java.util.*
 import kotlin.math.roundToInt
 
-class Field {
+class Field : Subject{
 
-    // TODO doba rustu
+    companion object {
+        private var observers: ArrayList<Observer> = ArrayList()
+    }
 
     private var crop : Crop
     private val capacity : Int
     private var isRaised : Boolean = true
+    private var uuid : UUID
 
     constructor(crop: Crop, capacity: Int) {
         this.crop = crop
         this.capacity = capacity
+        this.uuid = UUID.randomUUID()
+        notifyUpdate(this.uuid, "crop type: " + this.crop.getType().toString() + " , capacity " + this.capacity.toString())
     }
 
     fun isRaised() : Boolean{
@@ -36,5 +45,19 @@ class Field {
 
     fun getCrop() : Crop{
        return crop
+    }
+
+    override fun attach(o: Observer) {
+        Field.observers.add(o)
+    }
+
+    override fun detach(o: Observer) {
+        Field.observers.remove(o)
+    }
+
+    override fun notifyUpdate(uuid: UUID, report: String) {
+        for(i in Field.observers){
+            i.update(uuid, report)
+        }
     }
 }
